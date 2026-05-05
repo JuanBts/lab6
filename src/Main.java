6a565487b3d55b0f2a9e027545ae5c3c3216c8fe
busto <bustossegurajuanpablo6@gmail.com>
2026-05-05 16:35:48 -0400

    Lab6

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+        RegistroDisquera departamentoMarketing = RegistroDisquera.getInstance();
+        RegistroDisquera departamentoProduccion = RegistroDisquera.getInstance();
+
+
+        departamentoMarketing.registrarArtista("Dua Lipa");
+        departamentoMarketing.registrarArtista("The Weeknd");
+
+
+        departamentoProduccion.registrarArtista("Rosalía");
+
+
+        departamentoProduccion.mostrarArtistas();
+
+
+        System.out.println("¿Es la misma instancia?");
+        System.out.println(departamentoMarketing == departamentoProduccion);
     }
 }
